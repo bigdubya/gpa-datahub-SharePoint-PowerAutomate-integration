c717028855b8d89356bb00901f8ca582fcb70bf5
bigdubya <ade.ayeni@swiftstrategies.co.uk>
2023-08-04 17:36:35 +0100

    infra4

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,14 +1,32 @@
-resource "aws_s3_bucket" "test_bucket" {
-  bucket = var.s3_bucket_test_bucket_name
+###########################################################
+# AWS S3 bucket GPA Landing
+###########################################################
+resource "aws_s3_bucket" "gpa_landing" {
+  bucket = var.gpa_landing_bucket_name
+}
+
+resource "aws_s3_bucket_acl" "gpa_landing" {
+  bucket = aws_s3_bucket.gpa_landing.id
   acl    = "private"
+}
 
-  server_side_encryption_configuration {
-    rule {
-      bucket_key_enabled = false
+resource "aws_s3_bucket_public_access_block" "gpa_landing" {
+  bucket = aws_s3_bucket.gpa_landing.id
 
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "gpa_landing" {
+  bucket = aws_s3_bucket.gpa_landing.id
+
+  rule {
+    bucket_key_enabled = false
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,5 @@
-# global vars
-variable "region" {
+variable "gpa_landing_bucket_name" {
   type        = string
-  default     = "eu-west-2"
-  description = "AWS Region the S3 bucket should reside in"
-}
-
-# s3 vars
-variable "s3_bucket_test_bucket_name" {
-  description = "Name for the S3 bucket"
-  type        = string
+  default = "852536674014-terraform-test-bucket"
+  description = "Name of the s3 ingestion bucket"
 }
